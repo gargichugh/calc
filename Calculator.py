715a5ceabab05d64197bc6bbbf7b4e01fb59224a
GARGICHUGH\Gargi <chugh.gargi@gmail.com>
2017-02-05 13:55:34 -0800

    first commit

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+def calculator(a,b,oper):
+     if oper==1:
+        print(a+b)
+     elif oper==2:
+         print(a-b)
+     elif oper==3:
+        print(a*b)
+     elif oper==3:
+        print(a/b)
+
+print(calculator(1,2,3))
